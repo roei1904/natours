aa7423e88fb31544d4b9635440ea54de1b885076
roei shitrit <roei19765@gmail.com>
2026-02-02 13:32:46 +0200

    Remove test files and add config example

--- test_htt.js
+++
@@ -1,7 +0,0 @@
-const htmlToText = require('html-to-text');
-htmlToText;
-try {
-  ('Testing convert:', htmlToText.convert('<h1>Hello</h1>'));
-} catch (e) {
-  console.error('Error with convert:', e.message);
-}
